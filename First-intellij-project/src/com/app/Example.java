ac3d4d34630c92016798d3ed13d81eb0a708f84f
OdiljonKhasanov <okhasanov1995@gmail.com>
2019-06-16 13:22:14 -0700

    first commit

---
+++ First-intellij-project/src/com/app/Example.java
@@ -0,0 +1,13 @@
+package com.app;
+
+public class Example {
+
+    public static void main(String[] args) {
+
+        String str = "Hello";
+
+        System.out.println(str);
+        System.out.println(str.toUpperCase());
+
+    }
+}
