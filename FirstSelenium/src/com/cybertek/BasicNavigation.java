8ee26f43a58a221f2dc0ca3db7a6f9cc53cddfcd
OdiljonKhasanov <okhasanov1995@gmail.com>
2019-06-16 14:03:31 -0700

    added new message

--- FirstSelenium/src/com/cybertek/BasicNavigation.java
+++ FirstSelenium/src/com/cybertek/BasicNavigation.java
@@ -3,5 +3,6 @@ package com.cybertek;
 public class BasicNavigation {
     public static void main(String[] args) {
         System.out.println("Hello I am new class");
+        System.out.println("Some changes...");
     }
 }
